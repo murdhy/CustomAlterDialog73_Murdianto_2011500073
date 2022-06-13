0a0dc31dbceb23e1054d0b7f11dcf4e3469338c5
Fajrimulia <fajri.mulia.darma@gmail.com>
2022-06-13 05:56:39 -0800

    Initial commit

---
+++ app/src/main/java/com/example/customalterdialog73/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.customalterdialog73
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val btnkenalan = findViewById<Button>(R.id.btkenalan)
+
+        btnkenalan.setOnClickListener {
+            val inflateView = layoutInflater.inflate(R.layout.layout_alert,null)
+            val etNama =inflateView.findViewById<EditText>(R.id.etNama)
+            val ald = AlertDialog.Builder(this@MainActivity)
+            with(ald){
+                setTitle("Mau kenalan?")
+                setIcon(R.drawable.maskot_kotlin)
+                setView(inflateView)
+                setCancelable(false)
+                setPositiveButton("Selesai") {_, _ ->
+                    val  nama = if ("${etNama.text}".isNotEmpty()) "${etNama.text}" else "orang asing"
+                    Toast.makeText(this@MainActivity, "Salam kenal, $nama!", Toast.LENGTH_SHORT).show()
+                }
+                setNegativeButton("Siape lu") { _, _ ->
+                    Toast.makeText(this@MainActivity, "Sombong amat sih!", Toast.LENGTH_SHORT).show()
+                }
+                setNeutralButton("Maaf, cuma numpang lewat", null)//Tidak ada feedback dari aplikasih
+                create().show()
+            }
+        }
+    }
+}
